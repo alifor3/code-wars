e225e428366f02e4a9e8c669762731fccea12038
Ali's Macbook <Ali@alis-mbp.attlocal.net>
2019-04-11 17:49:06 -0500

    mrfreeze-codewars

--- javascript/Mr.Freeze.js
+++ javascript/Mr.Freeze.js
@@ -1,3 +1,4 @@
 // There is an object/class already created called MrFreeze. Mark this object as frozen so that no other changes can be made to it.
+// MrFreeze object exists
 
 Object.freeze(MrFreeze);
